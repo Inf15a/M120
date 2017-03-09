26139d6ec0cfa90386ea7f87832cd2ecf85806eb
Nici <Nici@Nicis-PC.home>
2017-03-09 22:16:35 +0100

    fix point and calculation bug on binary calc

--- ResponsiveCalculator/src/ch/lu/bbzw/calculator/view/BinaryCalculatorController.java
+++ ResponsiveCalculator/src/ch/lu/bbzw/calculator/view/BinaryCalculatorController.java
@@ -11,150 +11,152 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 public class BinaryCalculatorController extends DefaultCalculatorController {
-  @FXML
-  private GridPane binaryButtons;
-  @FXML
-  private RadioButton decimalChoice;
-  @FXML
-  private RadioButton binaryChoice;
-  private OutputChoice choice = OutputChoice.DECIMAL;
-  private int lastInput = 0;
+	@FXML
+	private GridPane binaryButtons;
+	@FXML
+	private RadioButton decimalChoice;
+	@FXML
+	private RadioButton binaryChoice;
+	private OutputChoice choice = OutputChoice.DECIMAL;
 
-  @Override
-  public void init() {
-    for (int i = 0; i < binaryButtons.getChildren().size(); i++) {
-      GridPane gridPane = (GridPane) binaryButtons.getChildren().get(i);
-      for (int j = 0; j < gridPane.getChildren().size(); j++) {
-        VBox vBox = (VBox) gridPane.getChildren().get(j);
-        ((Button) (vBox.getChildren().get(0))).setOnAction(new BinaryButtonAction());
-      }
-    }
+	@Override
+	public void init() {
+		for (int i = 0; i < binaryButtons.getChildren().size(); i++) {
+			GridPane gridPane = (GridPane) binaryButtons.getChildren().get(i);
+			for (int j = 0; j < gridPane.getChildren().size(); j++) {
+				VBox vBox = (VBox) gridPane.getChildren().get(j);
+				((Button) (vBox.getChildren().get(0))).setOnAction(new BinaryButtonAction());
+			}
+		}
 
-    txtOutput.setOnAction(event -> createBinaryButtonOutput());
-  }
+		calculatorInputField.addButtonActionListener(".", () -> {
+		});
 
-  @Override
-  protected void handleEqual() {
-    try {
-      switch (lastChoice) {
-        case PLUS:
-          int summand1 = lastInput;
-          int summand2 = Integer.parseInt(txtOutput.getText());
-          txtOutput.setText(String.valueOf(summand1 + summand2));
-          break;
-        case MINUS:
-          int minuend = lastInput;
-          int subtrahend = Integer.parseInt(txtOutput.getText());
-          txtOutput.setText(String.valueOf(minuend - subtrahend));
-          break;
-        case MULTIPLY:
-          int factor1 = lastInput;
-          int factor2 = Integer.parseInt(txtOutput.getText());
-          txtOutput.setText(String.valueOf(factor1 * factor2));
-          break;
-        case DIVIDE:
-          int dividend = lastInput;
-          int divisor = Integer.parseInt(txtOutput.getText());
-          txtOutput.setText(String.valueOf(dividend / divisor));
-          break;
-        default:
-          break;
-      }
-    } catch (NumberFormatException | NullPointerException e) {
-      // String is empty or no last input exists
-    }
-    createBinaryButtonOutput();
-  }
+		txtOutput.setOnAction(event -> createBinaryButtonOutput());
+	}
 
-  @FXML
-  private void handleDecimalBinaryChoice() {
-    if (decimalChoice.isSelected()) {
-      choice = OutputChoice.DECIMAL;
-      setDisabledToTextField2To9(false);
-      try {
-        txtOutput.setText(NumeralSystemConverterForStrings.txtToDecimal(txtOutput.getText()));
-      } catch (NumberFormatException e) {
-        // nothing has to be done, txtoutput is empty
-      }
-    } else if (binaryChoice.isSelected()) {
-      choice = OutputChoice.BINARY;
-      setDisabledToTextField2To9(true);
-      try {
-        txtOutput.setText(NumeralSystemConverterForStrings.txtToBinary(txtOutput.getText()));
-      } catch (NumberFormatException e) {
-        // nothing has to be done, txtoutput is empty
-      }
-    }
-  }
+	@Override
+	protected void handleEqual() {
+		try {
+			switch (lastChoice) {
+			case PLUS:
+				int summand1 = (int) lastInput;
+				int summand2 = Integer.parseInt(txtOutput.getText());
+				txtOutput.setText(String.valueOf(summand1 + summand2));
+				break;
+			case MINUS:
+				int minuend = (int) lastInput;
+				int subtrahend = Integer.parseInt(txtOutput.getText());
+				txtOutput.setText(String.valueOf(minuend - subtrahend));
+				break;
+			case MULTIPLY:
+				int factor1 = (int) lastInput;
+				int factor2 = Integer.parseInt(txtOutput.getText());
+				txtOutput.setText(String.valueOf(factor1 * factor2));
+				break;
+			case DIVIDE:
+				int dividend = (int) lastInput;
+				int divisor = Integer.parseInt(txtOutput.getText());
+				txtOutput.setText(String.valueOf(dividend / divisor));
+				break;
+			default:
+				break;
+			}
+		} catch (NumberFormatException | NullPointerException e) {
+			// String is empty or no last input exists
+		}
+		createBinaryButtonOutput();
+	}
 
-  private void setDisabledToTextField2To9(boolean disabled) {
-    calculatorInputField.changeFieldDisabled("2", disabled);
-    calculatorInputField.changeFieldDisabled("3", disabled);
-    calculatorInputField.changeFieldDisabled("4", disabled);
-    calculatorInputField.changeFieldDisabled("5", disabled);
-    calculatorInputField.changeFieldDisabled("6", disabled);
-    calculatorInputField.changeFieldDisabled("7", disabled);
-    calculatorInputField.changeFieldDisabled("8", disabled);
-    calculatorInputField.changeFieldDisabled("9", disabled);
-  }
+	@FXML
+	private void handleDecimalBinaryChoice() {
+		if (decimalChoice.isSelected()) {
+			choice = OutputChoice.DECIMAL;
+			setDisabledToTextField2To9(false);
+			try {
+				txtOutput.setText(NumeralSystemConverterForStrings.txtToDecimal(txtOutput.getText()));
+			} catch (NumberFormatException e) {
+				// nothing has to be done, txtoutput is empty
+			}
+		} else if (binaryChoice.isSelected()) {
+			choice = OutputChoice.BINARY;
+			setDisabledToTextField2To9(true);
+			try {
+				txtOutput.setText(NumeralSystemConverterForStrings.txtToBinary(txtOutput.getText()));
+			} catch (NumberFormatException e) {
+				// nothing has to be done, txtoutput is empty
+			}
+		}
+	}
 
-  private void createBinaryButtonOutput() {
-    char[] binaryCharArray;
-    if (choice == OutputChoice.DECIMAL) {
-      try {
-        binaryCharArray = new StringBuilder(Integer.toBinaryString(Integer.parseInt(txtOutput.getText()))).reverse()
-            .toString().toCharArray();
-      } catch (NumberFormatException e) {
-        return;
-      }
-    } else {
-      binaryCharArray = new StringBuilder(txtOutput.getText()).reverse().toString().toCharArray();
-    }
-    int index = 0;
-    for (int i = binaryButtons.getChildren().size() - 1; i >= 0; i--) {
-      GridPane gridPane = (GridPane) binaryButtons.getChildren().get(i);
-      for (int j = gridPane.getChildren().size() - 1; j >= 0; j--) {
-        VBox vBox = (VBox) gridPane.getChildren().get(j);
-        Button button = (Button) (vBox.getChildren().get(0));
-        try {
-          button.setText(String.valueOf(binaryCharArray[index]));
-        } catch (ArrayIndexOutOfBoundsException e) {
-          // if the char array is empty the fields have to be 0
-          button.setText("0");
-        }
-        index++;
-      }
-    }
-  }
+	private void setDisabledToTextField2To9(boolean disabled) {
+		calculatorInputField.changeFieldDisabled("2", disabled);
+		calculatorInputField.changeFieldDisabled("3", disabled);
+		calculatorInputField.changeFieldDisabled("4", disabled);
+		calculatorInputField.changeFieldDisabled("5", disabled);
+		calculatorInputField.changeFieldDisabled("6", disabled);
+		calculatorInputField.changeFieldDisabled("7", disabled);
+		calculatorInputField.changeFieldDisabled("8", disabled);
+		calculatorInputField.changeFieldDisabled("9", disabled);
+	}
 
-  private class BinaryButtonAction implements EventHandler<ActionEvent> {
+	private void createBinaryButtonOutput() {
+		char[] binaryCharArray;
+		if (choice == OutputChoice.DECIMAL) {
+			try {
+				binaryCharArray = new StringBuilder(Integer.toBinaryString(Integer.parseInt(txtOutput.getText())))
+						.reverse().toString().toCharArray();
+			} catch (NumberFormatException e) {
+				return;
+			}
+		} else {
+			binaryCharArray = new StringBuilder(txtOutput.getText()).reverse().toString().toCharArray();
+		}
+		int index = 0;
+		for (int i = binaryButtons.getChildren().size() - 1; i >= 0; i--) {
+			GridPane gridPane = (GridPane) binaryButtons.getChildren().get(i);
+			for (int j = gridPane.getChildren().size() - 1; j >= 0; j--) {
+				VBox vBox = (VBox) gridPane.getChildren().get(j);
+				Button button = (Button) (vBox.getChildren().get(0));
+				try {
+					button.setText(String.valueOf(binaryCharArray[index]));
+				} catch (ArrayIndexOutOfBoundsException e) {
+					// if the char array is empty the fields have to be 0
+					button.setText("0");
+				}
+				index++;
+			}
+		}
+	}
 
-    @Override
-    public void handle(ActionEvent event) {
-      Button button = (Button) event.getSource();
-      if (button.getText() == "1") {
-        button.setText("0");
-      } else {
-        button.setText("1");
-      }
-      createOutputFromButtons();
-    }
+	private class BinaryButtonAction implements EventHandler<ActionEvent> {
 
-    private void createOutputFromButtons() {
-      StringBuilder binarySB = new StringBuilder();
-      for (int i = 0; i < binaryButtons.getChildren().size(); i++) {
-        GridPane gridPane = (GridPane) binaryButtons.getChildren().get(i);
-        for (int j = 0; j < gridPane.getChildren().size(); j++) {
-          VBox vBox = (VBox) gridPane.getChildren().get(j);
-          binarySB.append(((Button) (vBox.getChildren().get(0))).getText());
-        }
-      }
-      if (choice == OutputChoice.BINARY) {
-        txtOutput.setText(binarySB.toString());
-      } else {
-        txtOutput.setText(String.valueOf(Integer.parseInt(binarySB.toString(), 2)));
-      }
-    }
-  }
+		@Override
+		public void handle(ActionEvent event) {
+			Button button = (Button) event.getSource();
+			if (button.getText() == "1") {
+				button.setText("0");
+			} else {
+				button.setText("1");
+			}
+			createOutputFromButtons();
+		}
+
+		private void createOutputFromButtons() {
+			StringBuilder binarySB = new StringBuilder();
+			for (int i = 0; i < binaryButtons.getChildren().size(); i++) {
+				GridPane gridPane = (GridPane) binaryButtons.getChildren().get(i);
+				for (int j = 0; j < gridPane.getChildren().size(); j++) {
+					VBox vBox = (VBox) gridPane.getChildren().get(j);
+					binarySB.append(((Button) (vBox.getChildren().get(0))).getText());
+				}
+			}
+			if (choice == OutputChoice.BINARY) {
+				txtOutput.setText(binarySB.toString());
+			} else {
+				txtOutput.setText(String.valueOf(Integer.parseInt(binarySB.toString(), 2)));
+			}
+		}
+	}
 
 }
--- ResponsiveCalculator/src/ch/lu/bbzw/calculator/view/DefaultCalculatorController.java
+++ ResponsiveCalculator/src/ch/lu/bbzw/calculator/view/DefaultCalculatorController.java
@@ -7,126 +7,126 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class DefaultCalculatorController {
-  @FXML
-  protected TextField txtOutput;
-  @FXML
-  protected CalculatorInputField calculatorInputField;
-  @FXML
-  protected CalculatorOperationsField calculatorOperationsField;
-  protected double lastInput = 0;
-  protected MathChoices lastChoice;
+	@FXML
+	protected TextField txtOutput;
+	@FXML
+	protected CalculatorInputField calculatorInputField;
+	@FXML
+	protected CalculatorOperationsField calculatorOperationsField;
+	protected double lastInput = 0;
+	protected MathChoices lastChoice;
 
-  @FXML
-  public void initialize() {
-    calculatorInputField.addButtonActionListener("0", this::handle0);
-    calculatorInputField.addButtonActionListener("1", () -> txtOutput.setText(txtOutput.getText() + "1"));
-    calculatorInputField.addButtonActionListener("2", () -> txtOutput.setText(txtOutput.getText() + "2"));
-    calculatorInputField.addButtonActionListener("3", () -> txtOutput.setText(txtOutput.getText() + "3"));
-    calculatorInputField.addButtonActionListener("4", () -> txtOutput.setText(txtOutput.getText() + "4"));
-    calculatorInputField.addButtonActionListener("5", () -> txtOutput.setText(txtOutput.getText() + "5"));
-    calculatorInputField.addButtonActionListener("6", () -> txtOutput.setText(txtOutput.getText() + "6"));
-    calculatorInputField.addButtonActionListener("7", () -> txtOutput.setText(txtOutput.getText() + "7"));
-    calculatorInputField.addButtonActionListener("8", () -> txtOutput.setText(txtOutput.getText() + "8"));
-    calculatorInputField.addButtonActionListener("9", () -> txtOutput.setText(txtOutput.getText() + "9"));
-    calculatorInputField.addButtonActionListener(".", this::handlePoint);
-    calculatorInputField.addButtonActionListener("C", this::handleDeleteLastToken);
+	@FXML
+	public void initialize() {
+		calculatorInputField.addButtonActionListener("0", this::handle0);
+		calculatorInputField.addButtonActionListener("1", () -> txtOutput.setText(txtOutput.getText() + "1"));
+		calculatorInputField.addButtonActionListener("2", () -> txtOutput.setText(txtOutput.getText() + "2"));
+		calculatorInputField.addButtonActionListener("3", () -> txtOutput.setText(txtOutput.getText() + "3"));
+		calculatorInputField.addButtonActionListener("4", () -> txtOutput.setText(txtOutput.getText() + "4"));
+		calculatorInputField.addButtonActionListener("5", () -> txtOutput.setText(txtOutput.getText() + "5"));
+		calculatorInputField.addButtonActionListener("6", () -> txtOutput.setText(txtOutput.getText() + "6"));
+		calculatorInputField.addButtonActionListener("7", () -> txtOutput.setText(txtOutput.getText() + "7"));
+		calculatorInputField.addButtonActionListener("8", () -> txtOutput.setText(txtOutput.getText() + "8"));
+		calculatorInputField.addButtonActionListener("9", () -> txtOutput.setText(txtOutput.getText() + "9"));
+		calculatorInputField.addButtonActionListener(".", this::handlePoint);
+		calculatorInputField.addButtonActionListener("C", this::handleDeleteLastToken);
 
-    calculatorOperationsField.addButtonActionListener("+", this::handleAddition);
-    calculatorOperationsField.addButtonActionListener("-", this::handleSubtraction);
-    calculatorOperationsField.addButtonActionListener("X", this::handleMultiplication);
-    calculatorOperationsField.addButtonActionListener("/", this::handleDivide);
-    calculatorOperationsField.addButtonActionListener("CE", this::handleClearAll);
-    calculatorOperationsField.addButtonActionListener("=", this::handleEqual);
+		calculatorOperationsField.addButtonActionListener("+", this::handleAddition);
+		calculatorOperationsField.addButtonActionListener("-", this::handleSubtraction);
+		calculatorOperationsField.addButtonActionListener("X", this::handleMultiplication);
+		calculatorOperationsField.addButtonActionListener("/", this::handleDivide);
+		calculatorOperationsField.addButtonActionListener("CE", this::handleClearAll);
+		calculatorOperationsField.addButtonActionListener("=", this::handleEqual);
 
-    init();
-  }
+		init();
+	}
 
-  public void init() {
-    // can be overwritten by subclasses always gets done as constructor
-  }
+	public void init() {
+		// can be overwritten by subclasses always gets done as constructor
+	}
 
-  protected void handle0() {
-    if (txtOutput.getText().length() > 0) {
-      txtOutput.setText(txtOutput.getText() + "0");
-    }
-  }
+	protected void handle0() {
+		if (txtOutput.getText().length() > 0) {
+			txtOutput.setText(txtOutput.getText() + "0");
+		}
+	}
 
-  protected void handlePoint() {
-    if (!txtOutput.getText().contains(".")) {
-      txtOutput.setText(txtOutput.getText() + ".");
-    }
-  }
+	protected void handlePoint() {
+		if (!txtOutput.getText().contains(".")) {
+			txtOutput.setText(txtOutput.getText() + ".");
+		}
+	}
 
-  protected void handleDeleteLastToken() {
-    try {
-      txtOutput.setText(txtOutput.getText().substring(0, txtOutput.getText().length() - 1));
-    } catch (StringIndexOutOfBoundsException e) {
-      // txtOutput is empty and c gets pressed, nothing has to be done
-    }
-  }
+	protected void handleDeleteLastToken() {
+		try {
+			txtOutput.setText(txtOutput.getText().substring(0, txtOutput.getText().length() - 1));
+		} catch (StringIndexOutOfBoundsException e) {
+			// txtOutput is empty and c gets pressed, nothing has to be done
+		}
+	}
 
-  protected void handleAddition() {
-    lastChoice = MathChoices.PLUS;
-    storLastInputAndEmptyTxtOutput();
-  }
+	protected void handleAddition() {
+		lastChoice = MathChoices.PLUS;
+		storLastInputAndEmptyTxtOutput();
+	}
 
-  protected void handleSubtraction() {
-    lastChoice = MathChoices.MINUS;
-    storLastInputAndEmptyTxtOutput();
-  }
+	protected void handleSubtraction() {
+		lastChoice = MathChoices.MINUS;
+		storLastInputAndEmptyTxtOutput();
+	}
 
-  protected void handleMultiplication() {
-    lastChoice = MathChoices.MULTIPLY;
-    storLastInputAndEmptyTxtOutput();
-  }
+	protected void handleMultiplication() {
+		lastChoice = MathChoices.MULTIPLY;
+		storLastInputAndEmptyTxtOutput();
+	}
 
-  protected void handleDivide() {
-    lastChoice = MathChoices.DIVIDE;
-    storLastInputAndEmptyTxtOutput();
-  }
+	protected void handleDivide() {
+		lastChoice = MathChoices.DIVIDE;
+		storLastInputAndEmptyTxtOutput();
+	}
 
-  protected void handleClearAll() {
-    txtOutput.setText("");
-    lastInput = 0;
-    lastChoice = null;
-  }
+	protected void handleClearAll() {
+		txtOutput.setText("");
+		lastInput = 0;
+		lastChoice = null;
+	}
 
-  protected void handleEqual() {
-    try {
-      switch (lastChoice) {
-        case PLUS:
-          double summand1 = lastInput;
-          double summand2 = Double.parseDouble(txtOutput.getText());
-          txtOutput.setText(String.valueOf(summand1 + summand2));
-          break;
-        case MINUS:
-          double minuend = lastInput;
-          double subtrahend = Double.parseDouble(txtOutput.getText());
-          txtOutput.setText(String.valueOf(minuend - subtrahend));
-          break;
-        case MULTIPLY:
-          double factor1 = lastInput;
-          double factor2 = Double.parseDouble(txtOutput.getText());
-          txtOutput.setText(String.valueOf(factor1 * factor2));
-          break;
-        case DIVIDE:
-          double dividend = lastInput;
-          double divisor = Double.parseDouble(txtOutput.getText());
-          txtOutput.setText(String.valueOf(dividend / divisor));
-          break;
-        default:
-          break;
-      }
-    } catch (NumberFormatException | NullPointerException e) {
-    }
-  }
+	protected void handleEqual() {
+		try {
+			switch (lastChoice) {
+			case PLUS:
+				double summand1 = lastInput;
+				double summand2 = Double.parseDouble(txtOutput.getText());
+				txtOutput.setText(String.valueOf(summand1 + summand2));
+				break;
+			case MINUS:
+				double minuend = lastInput;
+				double subtrahend = Double.parseDouble(txtOutput.getText());
+				txtOutput.setText(String.valueOf(minuend - subtrahend));
+				break;
+			case MULTIPLY:
+				double factor1 = lastInput;
+				double factor2 = Double.parseDouble(txtOutput.getText());
+				txtOutput.setText(String.valueOf(factor1 * factor2));
+				break;
+			case DIVIDE:
+				double dividend = lastInput;
+				double divisor = Double.parseDouble(txtOutput.getText());
+				txtOutput.setText(String.valueOf(dividend / divisor));
+				break;
+			default:
+				break;
+			}
+		} catch (NumberFormatException | NullPointerException e) {
+		}
+	}
 
-  protected void storLastInputAndEmptyTxtOutput() {
-    try {
-      lastInput = Double.parseDouble(txtOutput.getText());
-    } catch (NumberFormatException e) {
-      // string is already empty
-    }
-    txtOutput.setText("");
-  }
+	protected void storLastInputAndEmptyTxtOutput() {
+		try {
+			lastInput = Double.parseDouble(txtOutput.getText());
+		} catch (NumberFormatException e) {
+			// string is already empty
+		}
+		txtOutput.setText("");
+	}
 }
