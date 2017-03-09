f8a0400a6d4329c26c5b0225b3d7d16a10d66ca7
Nici <Nici@Nicis-PC.home>
2017-03-09 21:29:18 +0100

    add windows installer

--- ResponsiveCalculator/build/build/src/ch/lu/bbzw/calculator/view/RootLayoutController.java
+++ ResponsiveCalculator/build/build/src/ch/lu/bbzw/calculator/view/RootLayoutController.java
@@ -1,22 +1,33 @@
 package ch.lu.bbzw.calculator.view;
 
-import ch.lu.bbzw.calculator.MainApp;
+import ch.lu.bbzw.calculator.Calculator;
+import ch.lu.bbzw.calculator.utils.StyleType;
 import javafx.fxml.FXML;
 
 public class RootLayoutController {
-  private MainApp mainApp = null;
+	private Calculator calculator = null;
 
-  @FXML
-  private void handleDefaultCalculator() {
-    mainApp.showDefaultCalc();
-  }
+	@FXML
+	private void handleDefaultCalculator() {
+		calculator.showDefaultCalc();
+	}
 
-  @FXML
-  private void handleBinaryCalculator() {
-    mainApp.showBinaryCalc();
-  }
+	@FXML
+	private void handleBinaryCalculator() {
+		calculator.showBinaryCalc();
+	}
 
-  public void setMainApp(MainApp mainApp) {
-    this.mainApp = mainApp;
-  }
+	public void setMainApp(Calculator mainApp) {
+		this.calculator = mainApp;
+	}
+
+	@FXML
+	private void handleSetMaterialDesign() {
+		calculator.changeStylesheet(StyleType.MATERIAL);
+	}
+
+	@FXML
+	private void handleSetFXClassic() {
+		calculator.changeStylesheet(StyleType.FXCLASSIC);
+	}
 }
