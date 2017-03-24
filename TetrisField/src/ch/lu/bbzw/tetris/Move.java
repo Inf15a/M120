c66735539df4accb8b1b2137d683e455a8b87ed5
nicolas_schmid <nicolas_schmid@10.10.1.6>
2017-03-24 15:17:30 +0100

    add a new Block

--- TetrisField/src/ch/lu/bbzw/tetris/Move.java
+++ TetrisField/src/ch/lu/bbzw/tetris/Move.java
@@ -12,84 +12,77 @@ import ch.lu.bbzw.tetris.Tick.TickEndListener;
 import ch.lu.bbzw.tetris.blocks.Block;
 import ch.lu.bbzw.tetris.blocks.Block1X1;
 import ch.lu.bbzw.tetris.blocks.Block3X3;
+import ch.lu.bbzw.tetris.blocks.BlockL;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class Move implements Runnable {
-  private List<MoveEndListener> moveEndListeners = new ArrayList<>();
-  private TetrisField tetrisField;
+	private List<MoveEndListener> moveEndListeners = new ArrayList<>();
+	private TetrisField tetrisField;
+	private Block[] blocks = new Block[] { new Block1X1(), new Block3X3(), new BlockL() };
 
-  public Move(TetrisField tetrisField) {
-    this.tetrisField = tetrisField;
-  }
+	public Move(TetrisField tetrisField) {
+		this.tetrisField = tetrisField;
+	}
 
-  @Override
-  public void run() {
-    System.out.println("start move");
+	@Override
+	public void run() {
+		System.out.println("start move");
 
-    ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+		ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
-    Tick tick = new Tick(tetrisField);
-    tick.setActualBlock(generateRandomBlock());
-    tick.addTickEndListener(new TickEndListener() {
+		Tick tick = new Tick(tetrisField);
+		tick.setActualBlock(generateRandomBlock());
+		tick.addTickEndListener(new TickEndListener() {
 
-      @Override
-      public void onTickEnd(List<Point2D.Double> points, Block block, boolean isLastTick) {
-        tetrisField.setActiveBackground(points, block, isLastTick);
-        if (isLastTick) {
-          tetrisField.handleFullLines();
-          fireMoveEndListeners();
-          try {
-            scheduler.shutdown();
-            scheduler.awaitTermination(5, TimeUnit.SECONDS);
-          } catch (InterruptedException ignore) {
-            // do nothing
-          }
-        }
-      }
-    });
-    tetrisField.getScene().setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void onTickEnd(List<Point2D.Double> points, Block block, boolean isLastTick) {
+				tetrisField.setActiveBackground(points, block, isLastTick);
+				if (isLastTick) {
+					tetrisField.handleFullLines();
+					fireMoveEndListeners();
+					try {
+						scheduler.shutdown();
+						scheduler.awaitTermination(5, TimeUnit.SECONDS);
+					} catch (InterruptedException ignore) {
+						// do nothing
+					}
+				}
+			}
+		});
+		tetrisField.getScene().setOnKeyPressed(new EventHandler<KeyEvent>() {
 
-      @Override
-      public void handle(KeyEvent event) {
-        if (event.getCode() == KeyCode.LEFT) {
-          tick.moveLeft();
-        } else if (event.getCode() == KeyCode.RIGHT) {
-          tick.moveRight();
-        }
-      }
-    });
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.LEFT) {
+					tick.moveLeft();
+				} else if (event.getCode() == KeyCode.RIGHT) {
+					tick.moveRight();
+				}
+			}
+		});
 
-    scheduler.scheduleAtFixedRate(tick, 1, 1, TimeUnit.SECONDS);
-  }
+		scheduler.scheduleAtFixedRate(tick, 1, 1, TimeUnit.SECONDS);
+	}
 
-  public void addMoveEndListener(MoveEndListener endListener) {
-    moveEndListeners.add(endListener);
-  }
+	public void addMoveEndListener(MoveEndListener endListener) {
+		moveEndListeners.add(endListener);
+	}
 
-  private void fireMoveEndListeners() {
-    for (MoveEndListener moveEndListener : moveEndListeners) {
-      moveEndListener.onMoveEnd(tetrisField.checkIfFieldIsFinal(0, (TetrisField.amountOfColumns - 1) / 2));
-    }
-  }
+	private void fireMoveEndListeners() {
+		for (MoveEndListener moveEndListener : moveEndListeners) {
+			moveEndListener.onMoveEnd(tetrisField.checkIfFieldIsFinal(0, (TetrisField.amountOfColumns - 1) / 2));
+		}
+	}
 
-  private Block generateRandomBlock() {
-    Random randy = new Random();
-    switch (randy.nextInt(2)) {
-      case 0:
-        return new Block1X1();
-      case 1:
-        return new Block3X3();
-      default:
-        break;
-    }
-    return new Block() {
-    };
-  }
+	private Block generateRandomBlock() {
+		Random randy = new Random();
+		return blocks[randy.nextInt(blocks.length)];
+	}
 
-  @FunctionalInterface
-  interface MoveEndListener {
-    void onMoveEnd(boolean gameOver);
-  }
+	@FunctionalInterface
+	interface MoveEndListener {
+		void onMoveEnd(boolean gameOver);
+	}
 }
---
+++ TetrisField/src/ch/lu/bbzw/tetris/blocks/BlockL.java
@@ -0,0 +1,11 @@
+package ch.lu.bbzw.tetris.blocks;
+
+public class BlockL extends Block {
+	public BlockL() {
+		super();
+		pattern[0][1] = true;
+		pattern[1][1] = true;
+		pattern[2][1] = true;
+		pattern[2][2] = true;
+	}
+}
